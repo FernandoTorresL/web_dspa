dd24be789574964c0aa5374065a6bc7cc933c09a
Fernando Torres <fertorresmx@gmail.com>
2018-08-10 23:05:59 -0500

    Using ORM Eloquent to query DB

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,32 +1,16 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use App\Message;
 use Illuminate\Http\Request;
+
 class PagesController extends Controller
 {
     public function home()
     {
-        $messages = [
-            [
-                'id' => 1,
-                'content' => 'Este es mi primer mensaje!',
-                'image' => 'http://placeimg.com/600/338/woman?1',
-            ],
-            [
-                'id' => 2,
-                'content' => 'Este es mi segundo mensaje!',
-                'image' => 'http://placeimg.com/600/338/any',
-            ],
-            [
-                'id' => 3,
-                'content' => 'Otro mensaje más!',
-                'image' => 'https://picsum.photos/600/338?random',
-            ],
-            [
-                'id' => 4,
-                'content' => 'El último mensaje!',
-                'image' => 'https://picsum.photos/600/338?image=0',
-            ],
-        ];
+        $messages = Message::all();
+
         return view('welcome', [
             'messages' => $messages,
         ]);
