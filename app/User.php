c7bd7f0eae407df6d1f4c1875be312a557eac9d6
Fernando Torres <fertorresmx@gmail.com>
2018-09-27 00:14:44 -0500

    Add tables to implement Roles and Permissions

--- app/User.php
+++ app/User.php
@@ -37,6 +37,37 @@ class User extends Authenticatable
         return $this->belongsTo(Job::class);
     }
 
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function assignRole($role) {
+        $this->roles()->save(
+            Role::whereName($role)->firstOrFail()
+        );
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+
+        return !! $role->intersect($this->roles)-count();
+
+//        foreach ($role as $r)
+//        {
+//            if ($this->hasRole($r->name))
+//            {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+    }
+
+
     public function isAdminUser(User $user) {
         return $this->job->id == Job::find(1)->id || $this->job->id == Job::find(4)->id;
     }
