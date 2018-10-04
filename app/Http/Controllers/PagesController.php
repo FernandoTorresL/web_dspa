fd266f1699fbf228d5b162fc35727bee0c9f4334
Fernando Torres <fertorresmx@gmail.com>
2018-10-03 21:36:29 -0500

    Testing on RPBP3

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,13 +9,14 @@ class PagesController extends Controller
     public function home()
     {
         Log::info('Visitando WELCOME');
-//        $messages = Message::paginate(10);
+        $messages = Message::paginate(10);
 
         //Test login with another id
 //        Auth::LoginUsingID(2);
 
         return view('welcome', [
-//            'messages' => $messages,
+            'messages' => $messages,
         ]);
     }
 }
+
