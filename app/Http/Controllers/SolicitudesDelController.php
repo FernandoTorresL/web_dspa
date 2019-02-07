7da0a45028131b1edf0e0e1d5d9ff6c12dcaaaaa
Fernando Torres <fertorresmx@gmail.com>
2019-02-07 02:29:28 -0600

    Timeline solicitud (#48)
    * Add new page to view timeline for solicitudes
    
    * Moving code from timeline view to timeline Controller

--- app/Http/Controllers/SolicitudesDelController.php
+++ app/Http/Controllers/SolicitudesDelController.php
@@ -6,9 +6,19 @@ use App\Solicitud;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Gate;
+use Carbon\Carbon;
 
 class SolicitudesDelController extends Controller
 {
+
+    private function formatdate($pdate)
+    {
+        return Carbon::parse($pdate)->formatLocalized('%d de %B, %Y');
+    }
+
+    private function formatdatetime($pdatetime) {
+        return Carbon:: parse($pdatetime)->formatLocalized('%d de %B, %Y. %H:%M');
+    }
 
     //New function to show a sortable and simple table with pagination
     public function view_status()
@@ -48,6 +58,9 @@ class SolicitudesDelController extends Controller
 
     public function view_timeline($id)
     {
+        setlocale(LC_TIME, 'es_ES');
+        Carbon::setUtf8(false);
+      
         $del = Auth::user()->delegacion_id;
 
         Log::info('Ver timeline solicitudes. User: ' . Auth::user()->name . '|Del:' . $del);
@@ -55,9 +68,110 @@ class SolicitudesDelController extends Controller
         if (Gate::allows('ver_timeline_solicitudes')) {
             $datos_timeline = Solicitud::find($id);
 
-            //dd($datos_timeline);
+            $sin_dato = '--';
 
-            return view('ctas.solicitudes.timeline', compact('datos_timeline'));
+            $fecha_sol      = $this->formatdate($datos_timeline->fecha_solicitud_del);
+            $mov_id_sol     = $datos_timeline->movimiento_id;
+            $nombre_mov_sol = $datos_timeline->movimiento->name;
+            $cuenta_sol     = $datos_timeline->cuenta;
+            $matricula_sol  = $datos_timeline->matricula;
+            $curp_sol       = $datos_timeline->curp;
+            $nombre_sol     = $datos_timeline->primer_apellido . '-' . $datos_timeline->segundo_apellido . '-' . $datos_timeline->nombre;
+
+            $fecha_sol_cap      = $this->formatdate($datos_timeline->created_at);
+            $user_sol_cap       = $datos_timeline->user->name;
+            $comment_sol_cap    = $datos_timeline->comment;
+
+            $detalle_sol = '';
+            if( $mov_id_sol == 1 || $mov_id_sol == 4 ) //--If tipo_movimiento is ALTA or CONNECT--}}
+                $detalle_sol = $datos_timeline->gpo_nuevo->name;
+            elseif( $mov_id_sol == 2 ) //--If tipo_movimiento is BAJA--}}
+                $detalle_sol = $datos_timeline->gpo_actual->name;
+            elseif( $mov_id_sol == 3 ) //--If tipo_movimiento is CAMBIO--}}
+                $detalle_sol = $datos_timeline->gpo_actual->name . ' -> ' . $datos_timeline->gpo_nuevo->name;
+
+            $titulo_sol = $nombre_mov_sol . ' - ' . $cuenta_sol . ' (' . $detalle_sol . ')';
+
+            if( isset($datos_timeline->valija) ) {
+                $num_of_ca_val  = $datos_timeline->valija->num_oficio_ca;
+                $subt_val       = '';
+                $fecha_val      = $this->formatdate($datos_timeline->valija->fecha_valija_del);
+                $of_val         = $datos_timeline->valija->num_oficio_del;
+                $del_val        = $datos_timeline->delegacion->name;
+
+                $fecha_gestion      = $this->formatdate($datos_timeline->valija->fecha_recepcion_ca);
+                $num_gestion        = $datos_timeline->valija->num_oficio_ca;
+                $comment_gestion    = $datos_timeline->valija->comment;
+            }
+            else
+            {
+                $subt_val       = '(Solicitud sin valija)';
+                $num_of_ca_val  = $fecha_val = $of_val = $del_val = $sin_dato;
+                $fecha_gestion = $num_gestion = $comment_gestion  = $sin_dato;
+            }
+
+            //-- Setting the solicitud status --}}
+            if( isset($datos_timeline->rechazo) ) {
+                $color_sol_cap = 'text-danger';
+                $rechazo_sol_cap = 'No procede. ' . $datos_timeline->rechazo->full_name;
+            }
+            else {
+                $color_sol_cap = '';
+                $rechazo_sol_cap = $sin_dato;
+            }
+
+            if( isset($datos_timeline->lote) ) {
+                $fecha_lote     = $this->formatdate($datos_timeline->lote->fecha_oficio_lote);
+                $num_lote       = $datos_timeline->lote->num_lote;
+                $comment_lote   = $datos_timeline->lote->comment;
+            }
+            else {
+                $fecha_lote     = $num_lote = $comment_lote = $sin_dato;
+            }
+
+            if( isset($datos_timeline->resultado_solicitud) ) //--If solicitud has a response ... --}}
+            {
+                $nombre_resp    = $datos_timeline->resultado_solicitud->name;
+                $fecha_resp     = $this->formatdatetime($datos_timeline->resultado_solicitud->resultado_lote->attended_at);
+                $comment_resp   = $datos_timeline->resultado_solicitud->comment;
+
+                if( isset($datos_timeline->resultado_solicitud->rechazo_mainframe) ) {
+
+                    if( ($datos_timeline->resultado_solicitud->status == 1) ) {
+
+                        $color_resp = 'text-warning';
+                        $rechazo_resp = 'Pendiente. ';
+                    }
+                    else {
+                        $color_resp     = 'text-danger';
+                        $rechazo_resp   = 'No procede. ';
+                    }
+
+                    $cta_resp = $nombre_resp = $sin_dato;
+                    $rechazo_resp = $rechazo_resp . $datos_timeline->resultado_solicitud->rechazo_mainframe->name;
+                }
+                else {
+                    $cta_resp       = $datos_timeline->resultado_solicitud->cuenta;
+                    $color_resp     = 'text-success';
+                    $rechazo_resp   = 'Atendida.';
+                }
+            }
+            else {
+                $nombre_resp = $fecha_resp = $comment_resp = $cta_resp = $color_resp = $rechazo_resp = $sin_dato;
+            }
+
+            return view('ctas.solicitudes.timeline',
+                compact(
+                    'datos_timeline',
+                    'titulo_sol', 'fecha_sol', 'mov_id_sol', 'nombre_mov_sol', 'cuenta_sol',
+                    'matricula_sol', 'curp_sol', 'nombre_sol', 'detalle_sol', 'resultado_cta',
+                    'num_of_ca_val', 'subt_val', 'fecha_val', 'of_val', 'del_val',
+                    'fecha_gestion', 'num_gestion', 'comment_gestion',
+                    'fecha_sol_cap', 'user_sol_cap', 'comment_sol_cap',
+                    'color_sol_cap', 'rechazo_sol_cap',
+                    'fecha_lote', 'num_lote', 'comment_lote',
+                    'fecha_resp', 'cta_resp', 'nombre_resp', 'color_resp', 'rechazo_resp', 'comment_resp')
+                );
         }
         else {
             Log::info('Sin permiso-Consultar timeline solicitudes. Usuario:' . Auth::user()->name . '|Del:' . $del);
