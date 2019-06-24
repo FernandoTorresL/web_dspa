1c25097cfccfc31a90bbaf0372c1ba28d885e370
Fernando Torres <fertorresmx@gmail.com>
2019-06-24 15:17:54 -0500

    Decrease expiration time for token auth. Add a .env variable for this

--- config/auth.php
+++ config/auth.php
@@ -95,7 +95,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
-            'expire' => 60,
+            'expire' => env('TOKEN_EXPIRE_MIN'),
         ],
     ],
 
