c7c4e459f87036fd3e201a8c92332ae041b1b6ef
Fer Torres <fertorresmx@gmail.com>
2022-08-16 15:23:26 -0500

    Reuse code for export account list to file-Admin

--- app/Http/Controllers/ExportActiveAccountsDelController.php
+++ app/Http/Controllers/ExportActiveAccountsDelController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Subdelegacion;
+use App\Delegacion;
+
+use App\Http\Controllers\AccountsListController;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -10,7 +14,7 @@ use Illuminate\Support\Facades\Gate;
 class ExportActiveAccountsDelController extends Controller
 {
 
-    public function export_active_accounts_del ()
+    public function export_active_accounts_del ($p_delegacion_id)
     {
         $user_id = Auth::user()->id;
         $user_name = Auth::user()->name;
@@ -21,67 +25,12 @@ class ExportActiveAccountsDelController extends Controller
         $texto_log = 'User_id:' . $user_id . '|User:' . $user_name . '|Del:' . $user_del_id . '|Job:' . $user_job_id;
 
         //Si cuenta con los permisos...
-        if ( Gate::allows( 'export_lista_ctas_vigentes_del') )
+        if ( Auth::user()->hasRole('capturista_delegacional') && Gate::allows( 'export_lista_ctas_vigentes_del') && ($p_delegacion_id == $user_del_id) )
+
         {
-            $inventory_id = env('INVENTORY_ID');
-
-            // Get the account list from last inventory
-            $active_accounts_inventory =
-                DB::table('inventory_ctas AS IC')
-                ->join('groups AS G',       'IC.gpo_owner_id', '=', 'G.id')
-                ->join('inventories AS I',  'IC.inventory_id', '=', 'I.id')
-                ->leftjoin('work_areas AS W',  'IC.work_area_id', '=', 'W.id')
-                ->select(DB::Raw(
-                    'IC.cuenta          AS Cuenta,
-                    ""                  AS id,
-                    "Inventario"        AS Mov,
-                    IC.name             AS Nombre,
-                    G.name              AS Gpo_actual,
-                    "--"                AS Gpo_nuevo,
-                    "--"                AS Gpo_unificado,
-                    IC.install_data     AS Matricula,
-                    IC.work_area_id     AS Work_area_id,
-                    W.name              AS Work_area,
-                    I.cut_off_date      AS Fecha_mov'
-                ))
-            ->where('IC.inventory_id', $inventory_id);
-
-            // ...and the list of approved changes from Solicitudes
-            $active_accounts_solicitudes =
-                DB::table('solicitudes AS S')
-                ->join('resultado_solicitudes AS RS', 'S.id', '=', 'RS.solicitud_id')
-                ->join('resultado_lotes AS RL', 'RS.resultado_lote_id', '=', 'RL.id')
-                ->leftjoin('groups AS GA', 'S.gpo_actual_id', '=', 'GA.id')
-                ->leftjoin('groups AS GB', 'S.gpo_nuevo_id',  '=', 'GB.id')
-                ->join('movimientos AS M', 'S.movimiento_id', '=', 'M.id')
-                ->select(DB::Raw(
-                    'RS.cuenta      AS Cuenta,
-                    RS.solicitud_id AS id,
-                    M.name          AS Mov,
-                    concat(S.primer_apellido, " ", S.segundo_apellido, " ", S.nombre)
-                                    AS Nombre,
-                    GA.name         AS Gpo_actual,
-                    GB.name         AS Gpo_nuevo,
-                    "--"            AS Gpo_unificado,
-                    S.matricula     AS Matricula,
-                    0               AS Work_area_id,
-                    ""              AS Work_area,
-                    RL.attended_at  AS Fecha_mov'
-                ))
-            ->whereNull('RS.rechazo_mainframe_id');
-
-            //if is a 'Delegational' user, add delegacion_id to the query
-            if ( $user_del_id <> env('DSPA_USER_DEL_1') ) {
-                $active_accounts_inventory = $active_accounts_inventory->where('IC.delegacion_id', $user_del_id);
-                $active_accounts_solicitudes = $active_accounts_solicitudes->where('S.delegacion_id', $user_del_id);
-            }
-
-            //Finally, make UNION
-            $active_accounts = $active_accounts_solicitudes
-                ->union($active_accounts_inventory)
-                    ->orderby('Cuenta')
-                    ->orderby('Fecha_mov')
-                ->get();
+            $AccountsListController = new AccountsListController;
+            // Call to this function to get AccountList
+            $accounts_list_items = $AccountsListController->getAccountsListController($p_delegacion_id);
 
             // Filtrar los registros:
             $registro_anterior = NULL;
@@ -89,7 +38,7 @@ class ExportActiveAccountsDelController extends Controller
             $grupos_a_eliminar = explode(',', env('GROUPS_EXC'));
 
             // Check each record on this ordenated list (this is important, ORDER BY), and create new array with only active accounts
-            foreach ($active_accounts as $registro ) {
+            foreach ($accounts_list_items['active_accounts_list'] as $registro ) {
                 $registro_actual = $registro;
 
                 // If we have data to check on registro_anterior...
--- app/Http/Controllers/ExportActiveAccountsGralController.php
+++ app/Http/Controllers/ExportActiveAccountsGralController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Subdelegacion;
 use App\Delegacion;
+
+use App\Http\Controllers\AccountsListController;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -24,90 +27,17 @@ class ExportActiveAccountsGralController extends Controller
         //Si cuenta con los permisos...
         if ( Auth::user()->hasRole('admin_dspa') && Gate::allows('export_lista_ctas_vigentes_gral') )
         {
-            $inventory_id = env('INVENTORY_ID');
-
-            // Get Delegaciones (OOAD's)
-            $delegaciones_gral_list =
-                Delegacion::where('status', '<>', 0)
-                    ->orderBy('id', 'asc')
-                    ->get();
-
-            $delegacion_a_consultar =
-                Delegacion::find($p_delegacion_id);
-
-            // Get the account list from last inventory
-            $active_accounts_gral_inventory =
-                DB::table('inventory_ctas AS IC')
-                ->join('delegaciones AS D',   'IC.delegacion_id', '=', 'D.id')
-                ->join('groups AS G',         'IC.gpo_owner_id',  '=', 'G.id')
-                ->join('inventories AS I',    'IC.inventory_id',  '=', 'I.id')
-                ->leftjoin('work_areas AS W', 'IC.work_area_id',  '=', 'W.id')
-                ->select(DB::Raw(
-                    'IC.cuenta      AS Cuenta,
-                    ""              AS Id,
-                    D.id            AS Del_id,
-                    D.name          AS Del_name,
-                    "Inventario"    AS Mov,
-                    IC.name         AS Nombre,
-                    G.name          AS Gpo_actual,
-                    "--"            AS Gpo_nuevo,
-                    "--"            AS Gpo_unificado,
-                    IC.install_data AS Matricula,
-                    IC.work_area_id AS Work_area_id,
-                    W.name          AS Work_area_name,
-                    I.cut_off_date  AS Fecha_mov'
-                ))
-            ->where('IC.inventory_id', $inventory_id);
-
-            // ...and the list of approved changes from Solicitudes
-            $active_accounts_gral_solicitudes =
-                DB::table('solicitudes AS S')
-                ->join('delegaciones AS D',           'S.delegacion_id',      '=', 'D.id')
-                ->join('resultado_solicitudes AS RS', 'RS.solicitud_id',       '=', 'S.id')
-                ->join('resultado_lotes AS RL',       'RS.resultado_lote_id', '=', 'RL.id')
-                ->leftjoin('groups AS GA',            'S.gpo_actual_id',      '=', 'GA.id')
-                ->leftjoin('groups AS GB',            'S.gpo_nuevo_id',       '=', 'GB.id')
-                ->join('movimientos AS M',            'S.movimiento_id',      '=', 'M.id')
-                ->select(DB::Raw(
-                    'RS.cuenta      AS Cuenta,
-                    RS.solicitud_id AS Id,
-                    D.id            AS Del_id,
-                    D.name          AS Del_name,
-                    M.name          AS Mov,
-                    concat(S.primer_apellido, "-", S.segundo_apellido, "-", S.nombre)
-                                    AS Nombre,
-                    GA.name         AS Gpo_actual,
-                    GB.name         AS Gpo_nuevo,
-                    "--"            AS Gpo_unificado,
-                    S.matricula     AS Matricula,
-                    0               AS Work_area_id,
-                    ""              AS Work_area_name,
-                    RL.attended_at  AS Fecha_mov'
-                ))
-            ->whereNull('RS.rechazo_mainframe_id');
-
-            //if we had a parameter var 'Delegational_id', add delegacion_id to the query
-            if ( $delegacion_a_consultar->id <> 0 ) {
-                $active_accounts_gral_inventory =
-                    $active_accounts_gral_inventory->where('IC.delegacion_id', $delegacion_a_consultar->id);
-                $active_accounts_gral_solicitudes =
-                    $active_accounts_gral_solicitudes->where('S.delegacion_id', $delegacion_a_consultar->id);
-            }
-
-            //Finally, make UNION
-            $active_accounts_gral = $active_accounts_gral_solicitudes
-                ->union($active_accounts_gral_inventory)
-                    ->orderby('Cuenta')
-                    ->orderby('Fecha_mov')
-                ->get();
+            $AccountsListController = new AccountsListController;
+            // Call to this function to get AccountList
+            $accounts_list_items = $AccountsListController->getAccountsListController($p_delegacion_id);
 
             // Filtrar los registros:
             $registro_anterior = NULL;
-            $active_accounts_gral_list = [];
+            $active_accounts_list = [];
             $grupos_a_eliminar = explode(',', env('GROUPS_EXC'));
 
             // Check each record on this ordenated list (this is important, ORDER BY), and create new array with only active accounts
-            foreach ($active_accounts_gral as $registro ) {
+            foreach ($accounts_list_items['active_accounts_list'] as $registro ) {
                 $registro_actual = $registro;
 
                 // If we have data to check on registro_anterior...
@@ -126,7 +56,7 @@ class ExportActiveAccountsGralController extends Controller
                             // It's not BAJA and it's an Afiliación Group, we keep this record on the new array
                             if  ( !( in_array($registro_anterior->Gpo_unificado, $grupos_a_eliminar) ) ) {
                                 // Finally, add the record data to the final list
-                                array_push($active_accounts_gral_list, $registro_anterior);
+                                array_push($active_accounts_list, $registro_anterior);
 
                             }
                         }
@@ -134,7 +64,7 @@ class ExportActiveAccountsGralController extends Controller
                     // If there's a connect, it has to be added too
                     else if ($registro_anterior->Mov == "CONNECT") {
                         $registro_anterior->Gpo_unificado = $registro_anterior->Gpo_nuevo;
-                        array_push($active_accounts_gral_list, $registro_anterior);
+                        array_push($active_accounts_list, $registro_anterior);
                     }
                 }
                 $registro_anterior = $registro_actual;
@@ -148,52 +78,8 @@ class ExportActiveAccountsGralController extends Controller
 
         Log::info('Exportar Lista Ctas Vigentes Nacional|' . $texto_log);
 
-        $var = 1;
-        $delimiter = ",";
-        $num_delegacion = str_pad($delegacion_a_consultar->id, 2, '0', STR_PAD_LEFT);
-
-        if ( $delegacion_a_consultar->id == 0 )
-            $filename = "ADMIN-Nacional-CtasVig " . date('dMY H:i:s') . ".csv";
-        else
-            $filename = "ADMIN-" . $num_delegacion . "-CtasVig " . date('dMY H:i:s') . ".csv";
-
-        // Create a file pointer
-        $f = fopen('php://memory', 'w');
-
-        // Set column headers
-        $fields = array('#', 'USER-ID', 'Id', 'Del_id', 'Del_name', 'Origen', 'Nombre', 'Grupo actual', 'Gpo_nuevo', 'Gpo_unificado', 'Matrícula', 'work_area_id', 'work_area_name', 'Fecha_mov');
-        fputcsv($f, $fields, $delimiter);
-
-        foreach ( $active_accounts_gral_list as $row_active_accounts ) {
-            $lineData = array(
-                $var,
-                $row_active_accounts->Cuenta,
-                $row_active_accounts->Id,
-                $row_active_accounts->Del_id,
-                $row_active_accounts->Del_name,
-                $row_active_accounts->Mov,
-                $row_active_accounts->Nombre,
-                $row_active_accounts->Gpo_actual,
-                $row_active_accounts->Gpo_nuevo,
-                $row_active_accounts->Gpo_unificado,
-                $row_active_accounts->Matricula,
-                $row_active_accounts->Work_area_id,
-                $row_active_accounts->Work_area_name,
-                $row_active_accounts->Fecha_mov,
-            );
-
-            fputcsv($f, $lineData, $delimiter);
-            $var += 1;
-        }
-
-        // Move back to beginning of file
-        fseek($f, 0);
-
-        // Set headers to download file rather than displayed
-        header('Content-Type: text/csv');
-        header('Content-Disposition: attachment; filename="' . $filename . '";');
-
-        // Output all remaining data on a file pointer
-        fpassthru($f);
+        $AccountsListController = new AccountsListController;
+        // Call to method to export accountlist
+        $AccountsListController->downloadAccountsList($active_accounts_list, $p_delegacion_id, FALSE);
     }
 }
